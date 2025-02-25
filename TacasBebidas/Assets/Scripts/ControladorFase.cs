478e31fa5258173eb35b0f756b9a7f7871fd08ba
ERICK OLIVEIRA DE SOUZA <erick.osouza1@senacsp.edu.br>
2025-02-25 17:34:36 -0300

    aula 25-02

---
+++ TacasBebidas/Assets/Scripts/ControladorFase.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ControladorFase : MonoBehaviour
+{
+    internal float TempoRestante;
+
+    public GameObject telaGanhou, TelaPerdeuErrou; telaPerdeuTempo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
